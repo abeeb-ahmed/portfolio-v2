8468b750b358321bba674718cb6c2612d50d2854
Abeeb Ahmed <abeebahmed@gmail.com>
2022-03-12 13:58:19 +0100

    Added Scroll down to laptop view

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="header__right"></div>
+        {/* <div className="header__right"></div> */}
       </div>
     </div>
   );
